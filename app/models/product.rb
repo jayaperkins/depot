b448eeff307f8ffa6d788610fff4f2ef1ca0e28c
jayaperkins <jayaperkins@gmail.com>
2016-02-18 18:07:16 -0800

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, :price, presence: true
+
+  validates :price,
+            numericality: {greater_than_or_equal_to: 0.01}
+
+  validates :title,
+            uniqueness: true
+
+  validates :image_url, allow_blank: true, format:  {
+      with: %r{\.(gif|jpg|png)\Z}i,
+      message: "must end with .gif, .jpg, or .png"
+  }
+
+end
