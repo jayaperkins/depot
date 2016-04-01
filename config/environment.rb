c82f56f4a91fad6995b5e3402d3994aa2300cf63
jayaperkins <jayaperkins@gmail.com>
2016-03-31 20:49:02 -0700

    week 11 work

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      authentication: 'plain',
+      user_name: 'jayaaaronperkins@gmail.com',
+      password: 'skandar15',
+      enable_starttls_auto: true
+  }
+end
